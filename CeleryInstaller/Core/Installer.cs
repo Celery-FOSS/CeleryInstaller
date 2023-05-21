b62d591ce427d82084b0cf10cffc954828b040a4
Astro <57644081+astroctrl@users.noreply.github.com>
2023-05-21 16:31:17 -0300

    Update Installer.cs

--- CeleryInstaller/Core/Installer.cs
+++ CeleryInstaller/Core/Installer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.IO.Compression;
@@ -12,38 +12,81 @@ namespace CeleryInstaller.Core
 {
     public static class Installer
     {
+        private static async Task<string> GetLatestRelease(string url)
+        {
+            string json = await App.HttpClient.GetStringAsync(url);
+
+            if (string.IsNullOrEmpty(json))
+                throw new Exception("Failed to obtain information for the New UI download!");
+
+            GithubRelease releaseInformation = json.FromJson<GithubRelease>();
+
+            if (releaseInformation == null)
+                throw new Exception("Failed to obtain release information!");
+
+            Asset targetAsset = releaseInformation.assets.First(x => x.content_type == "application/x-zip-compressed");
+            return targetAsset.browser_download_url;
+        }
+
+        public static async Task CopyToAsync(this Stream source, Stream destination, int bufferSize, IProgress<long> progress = null, CancellationToken cancellationToken = default)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (!source.CanRead)
+                throw new ArgumentException("Has to be readable", nameof(source));
+            if (destination == null)
+                throw new ArgumentNullException(nameof(destination));
+            if (!destination.CanWrite)
+                throw new ArgumentException("Has to be writable", nameof(destination));
+            if (bufferSize < 0)
+                throw new ArgumentOutOfRangeException(nameof(bufferSize));
+
+            byte[] buffer = new byte[bufferSize];
+            long totalBytesRead = 0;
+            int bytesRead;
+            while ((bytesRead = await source.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) != 0)
+            {
+                await destination.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
+                totalBytesRead += bytesRead;
+                progress?.Report(totalBytesRead);
+            }
+        }
+        private static async Task DownloadAsync(this HttpClient client, string requestUri, Stream destination, IProgress<string> progressString, IProgress<float> progressFloat, CancellationToken cancellationToken = default)
+        {
+            progressString.Report("Initializing download...");
+            using (HttpResponseMessage response = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead))
+            {
+                long? contentLength = response.Content.Headers.ContentLength;
+                using (Stream downloadStream = await response.Content.ReadAsStreamAsync())
+                {
+                    if (!contentLength.HasValue)
+                    {
+                        await downloadStream.CopyToAsync(destination);
+                        return;
+                    }
+
+                    IProgress<long> relativeProgress = new Progress<long>(totalBytes =>
+                    {
+                        float progress = (float)totalBytes / contentLength.Value;
+                        progressFloat.Report(progress);
+                        progressString.Report($"Downloading... {Math.Round(progress * 100)}%");
+                    });
+
+                    await downloadStream.CopyToAsync(destination, 81920, relativeProgress, cancellationToken);
+                    progressString.Report("Download Complete!");
+                }
+            }
+        }
+
         public static async Task InstallLatestVersion(Configuration configuration, IProgress<string> progressString, IProgress<float> progressFloat)
         {
             if (!Directory.Exists(configuration.InstallLocation))
                 Directory.CreateDirectory(configuration.InstallLocation);
 
-            string url = "";
-            switch (configuration.PreferedExecutor)
-            {
-                case Configuration.ExecutorPreference.LegacyUI:
-                    url = "https://cdn.sussy.dev/celery/release.zip";
-                    break;
-                case Configuration.ExecutorPreference.NewUI:
-                    string json = await App.HttpClient.GetStringAsync("https://api.github.com/repos/sten-code/Celery/releases/latest");
-
-                    if (string.IsNullOrEmpty(json))
-                        throw new Exception("Failed to obtain information for the New UI download!");
-
-                    GithubRelease releaseInformation = json.FromJson<GithubRelease>();
-
-                    if (releaseInformation == null)
-                        throw new Exception("Failed to obtain release information!");
-
-                    // Get the first zip that this release contains.
-                    Asset targetAsset = releaseInformation.assets.First(x => x.content_type == "application/x-zip-compressed");
-                    url = targetAsset.browser_download_url;
-                    break;
-                default:
-                    break;
-            }
+            string url = (configuration.PreferedExecutor == Configuration.ExecutorPreference.LegacyUI) ? "https://cdn.sussy.dev/celery/release.zip" : (await GetLatestRelease("https://api.github.com/repos/sten-code/Celery/releases/latest"));
 
             if (url == "")
-                return;
+                throw new Exception("Failed to fetch the download url!");
 
             using (MemoryStream memStream = new(8000000)) // 8000000 == 8 Megabytes
             {
@@ -74,57 +117,6 @@ namespace CeleryInstaller.Core
                     }
                     progressString.Report("Extraction Complete!");
                 });
-            }
-        }
-
-        public static async Task DownloadAsync(this HttpClient client, string requestUri, Stream destination, IProgress<string> progressString, IProgress<float> progressFloat, CancellationToken cancellationToken = default)
-        {
-            progressString.Report("Initializing download...");
-            using (HttpResponseMessage response = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead))
-            {
-                long? contentLength = response.Content.Headers.ContentLength;
-                using (Stream downloadStream = await response.Content.ReadAsStreamAsync())
-                {
-                    if (!contentLength.HasValue)
-                    {
-                        await downloadStream.CopyToAsync(destination);
-                        return;
-                    }
-
-                    IProgress<long> relativeProgress = new Progress<long>(totalBytes => 
-                    {
-                        float progress = (float)totalBytes / contentLength.Value;
-                        progressFloat.Report(progress);
-                        progressString.Report($"Downloading... {Math.Round(progress * 100)}%");
-                    });
-
-                    await downloadStream.CopyToAsync(destination, 81920, relativeProgress, cancellationToken);
-                    progressString.Report("Download Complete!");
-                }
-            }
-        }
-
-        public static async Task CopyToAsync(this Stream source, Stream destination, int bufferSize, IProgress<long> progress = null, CancellationToken cancellationToken = default)
-        {
-            if (source == null)
-                throw new ArgumentNullException(nameof(source));
-            if (!source.CanRead)
-                throw new ArgumentException("Has to be readable", nameof(source));
-            if (destination == null)
-                throw new ArgumentNullException(nameof(destination));
-            if (!destination.CanWrite)
-                throw new ArgumentException("Has to be writable", nameof(destination));
-            if (bufferSize < 0)
-                throw new ArgumentOutOfRangeException(nameof(bufferSize));
-
-            byte[] buffer = new byte[bufferSize];
-            long totalBytesRead = 0;
-            int bytesRead;
-            while ((bytesRead = await source.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) != 0)
-            {
-                await destination.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
-                totalBytesRead += bytesRead;
-                progress?.Report(totalBytesRead);
             }
         }
 
